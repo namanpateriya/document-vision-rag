ab056a0693f633701b4fc1527713d3df226b8791
namanpateriya <pateriyanaman@gmail.com>
2026-05-18 17:47:18 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,19 +3,19 @@ from fastapi import FastAPI
 from app.service import DocumentService
 
 app = FastAPI(
-    title="Document Vision RAG - Step 1"
+    title="Document Vision RAG - Step 2"
 )
 
 
 @app.get("/")
 def health():
-
-    return {
-        "status": "running"
-    }
+    return {"status": "running"}
 
 
-@app.get("/process")
-def process(file_path: str):
+@app.get("/query")
+def query(file_path: str, query: str):
 
-    return DocumentService.process(file_path)
+    return DocumentService.process(
+        file_path,
+        query
+    )
